0670704dd79407f81ab709f5fec21d7cad9a858b
HEMANTHKUMARANNAM <hemanthkumar312004@gmail.com>
2025-05-02 11:11:55 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from Azure!' });
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
